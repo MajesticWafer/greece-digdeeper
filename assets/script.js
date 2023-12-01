6af083cbce9809d1a925a65357fe14a59308338b
MajesticWafer <123651667+MajesticWafer@users.noreply.github.com>
2023-12-01 12:13:00 -0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,12 @@
 $(document).ready(function() {
-  // Function to check if an element is in the viewport
-  function isElementInViewport(elem) {
-    var rect = elem.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
-
-  // Function to add a class when an element is in the viewport
-  function addClassOnScroll() {
+  $(window).on('scroll', function() {
     $('.section').each(function() {
-      if (isElementInViewport(this)) {
-        $(this).addClass('visible');
+      var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      if (bottom_of_window > bottom_of_element) {
+        $(this).animate({'opacity':'1', 'margin-top':'0px'}, 1000);
       }
     });
-  }
-
-  // Add the 'visible' class to elements in the viewport on page load
-  addClassOnScroll();
-
-  // Add the 'visible' class to elements in the viewport on scroll
-  $(window).on('scroll', function() {
-    addClassOnScroll();
   });
 });
